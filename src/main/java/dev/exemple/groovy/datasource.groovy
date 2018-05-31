c876939a6224c772ac9652eb6975215d598b91a8
Valentin Bourdaud <bourdaudv@gmail.com>
2018-05-31 17:24:34 +0200

    TP termine

---
+++ src/main/java/dev/exemple/groovy/datasource.groovy
@@ -0,0 +1,30 @@
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+
+
+@Configuration
+
+@Grab(group='mysql', module='mysql-connector-java', version='5.1.41')
+
+class DataSourceConfig {
+
+@Bean
+
+public DataSource dataSource() {
+
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+
+    dataSource.setUrl("jdbc:mysql://localhost:3306/sirh-paie");
+
+    dataSource.setUsername("root");
+
+    dataSource.setPassword("");
+
+    return dataSource;
+    }
+
+}
